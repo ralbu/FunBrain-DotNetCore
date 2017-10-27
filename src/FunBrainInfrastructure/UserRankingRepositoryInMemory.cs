bf457285ca23587b85faeb546595090605179a36
ruslan <ruslan.albu@hotmail.com>
2017-10-27 07:03:55 +0100

    in memory ranking

---
+++ src/FunBrainInfrastructure/UserRankingRepositoryInMemory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FunBrainDomain;
+
+namespace FunBrainInfrastructure
+{
+    public class UserRankingRepositoryInMemory: IUserRankingRepository
+    {
+        public IEnumerable<UserRanking> Get()
+        {
+            yield return new UserRanking
+            {
+                Id = 1,
+                Name = "User 1",
+                Ranking = 20
+            };
+            yield return new UserRanking
+            {
+                Id = 2,
+                Name = "User 2",
+                Ranking = 30
+            };
+        }
+
+        public void UpdateWinner(int userId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
