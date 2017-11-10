c5fd1542158d2f56f8d47485f8537beb38481350
ruslan <ruslan.albu@hotmail.com>
2017-11-10 06:53:35 +0000

    Controllers for game

---
+++ src/FunBrainApi/Controllers/GameController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FunBrainInfrastructure;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunBrainApi.Controllers
+{
+    [Route("api/game")]
+    public class GameController: Controller
+    {
+
+        public IActionResult Get()
+        {
+            return Ok(new {Game = 1, UserName = "tarsats"});
+        }
+
+
+        [HttpPost("start")]
+        public IActionResult Start([FromBody] GameInput gameInput)
+        {
+            return Ok(gameInput);
+        }
+
+
+        [HttpPost("run")]
+        public IActionResult Run([FromBody] IEnumerable<UserInGame> usersInGame)
+        {
+            return Ok(usersInGame);
+        }
+    }
+}
---
+++ src/FunBrainInfrastructure/UserInGame.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunBrainInfrastructure
+{
+    public class UserInGame
+    {
+        public int UserId { get; set; }
+        public int Number { get; set; }
+    }
+}
